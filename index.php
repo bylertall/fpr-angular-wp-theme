233d16c21d942a9531608c59f41c4f08559ebfa7
bylertall <tball88@gmail.com>
2015-07-08 12:54:47 -0700

    move ngapp to fix wpadmin bar, fix collapsing margin between post headers in feed

--- index.php
+++ index.php
@@ -1,20 +1,22 @@
 <!DOCTYPE html>
-<html <?php language_attributes();?> ng-app="fprApp">
+<html <?php language_attributes();?>>
 	<head>
 		<base href="<?php $url_info = parse_url( home_url() ); echo trailingslashit( $url_info['path'] ); ?>">
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge"
-		<?php wp_head(); ?>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<title>The Fancy Pants Report | A San Francisco Style Blog by Kate Ogata</title>
+		<?php wp_head(); ?>
 	</head>
 
 	<body>
-		<header></header>
-		<main ui-view></main>
-		<footer>
-			&copy; <?php echo date( 'Y' ); ?>
-		</footer>
+		<div class="angular-wrapper" ng-app="fprApp">
+			<header>INSTA AND NAV WILL GO HERE</header>
+			<main ui-view></main>
+			<footer>
+				&copy; <?php echo date( 'Y' ); ?>
+			</footer>
+		</div>
 
 		<?php wp_footer(); ?>
 	</body>
