8bd4a8ad65987ba43cbd44ae23f0670a73ff1200
bylertall <tball88@gmail.com>
2015-08-21 07:54:39 -0700

    clean up search component

--- src/app/components/search/search.controller.js
+++ src/app/components/search/search.controller.js
@@ -11,38 +11,49 @@ function Search(wpService) {
         s: ''
     };
 
+    // initial config
+    vm.posts = [];
     vm.currentSearchString = '';
     vm.resultsActive = false;
     vm.showError = false;
     vm.loadingMorePosts = false;
     vm.noMoreResults = false;
 
+    // get search results
     vm.getInitialResults = function($event) {
         if ($event.which == 13) {
+
+            // if empty string is entered
+            if (vm.filter.s == '') {
+                vm.resultsActive = false;
+                return vm.posts = [];
+            }
+
+            // reset vars if new search
+            vm.showError = false;
+            vm.noMoreResults = false;
             vm.resultsActive = true;
             vm.loadingMorePosts = true;
             _getSearchResults(wpService, vm.filter.s).success(function(res) {
                 if (res) {
-                    vm.resultsActive = true;
-                    vm.posts = wpService.searchResults;
-                    vm.noMoreResults = res.length < 20;
-                    console.log(vm.noMoreResults);
                     vm.currentSearchString = vm.filter.s;
+                    vm.posts = wpService.searchResults;
+                    vm.resultsActive = true;
                     vm.showError = res.length === 0;
+                    vm.noMoreResults = res.length < 20;
                     vm.loadingMorePosts = false;
                 }
             });
         }
     };
 
+    // get more search results (load more button)
     vm.getMoreResults = function() {
         vm.loadingMorePosts = true;
         _getSearchResults(wpService, vm.currentSearchString).success(function(res) {
             if (res) {
                 vm.posts.concat(wpService.searchResults);
                 vm.noMoreResults = res.length < 20;
-                console.log(res.length);
-                console.log(vm.noMoreResults);
                 vm.loadingMorePosts = false;
             }
         });
