5e8cb5be64f5665beaedec1ef2000e3a05e171c5
Tyler Ball <tylerball.dev@gmail.com>
2015-11-15 21:24:11 -0800

    add img preload directive

---
+++ src/app/components/helpers/preload-image/preloadImg.directive.js
@@ -0,0 +1,35 @@
+// inspired by http://www.bennadel.com/blog/2597-preloading-images-in-angularjs-with-promises.htm
+// and https://github.com/RevillWeb/angular-preload-image
+
+angular
+    .module('fprApp')
+    .directive('fprPreloadImg', fprPreloadImg);
+
+function fprPreloadImg() {
+    var directive = {
+        scope: {
+            fprPreloadImg: '@'
+        },
+        link: link
+    };
+
+    return directive;
+
+    function link(scope, elem, attrs) {
+        var imgUrl = scope.fprPreloadImg;
+        // if background img url is NOT false or empty string, set background img url, then emit ready
+        // otherwise, just emit ready to  remove loading
+        preLoader(imgUrl, function(img) {
+            elem.append(img);
+            elem.css({'opacity': '1'});
+        });
+    }
+
+    function preLoader(url, successCallback) {
+        var img = angular.element(new Image());
+
+        img.bind('load', function() {
+            successCallback(img);
+        }).attr('src', url);
+    }
+}
